76b670676b0d75a73bf08ff5dc1148e502f5c93b
John Lovett <admin@jplovett.net>
2025-10-22 10:49:30 -0400

    dropped postcss, parcel handles minify

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-preset-env')({ stage: 3 }),
-    ...(process.env.NODE_ENV === 'production'
-      ? [require('cssnano')({ preset: 'default' })]
-      : [])
-  ]
-};
